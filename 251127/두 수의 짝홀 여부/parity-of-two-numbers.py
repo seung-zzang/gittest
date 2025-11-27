a979ec434e06745cb4e0dd2ac0f30926897d4b6f
seung-zzang <164981656+seung-zzang@users.noreply.github.com>
2025-11-27 10:53:16 +0900

    Title: 두 수의 짝홀 여부, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251127/두 수의 짝홀 여부/parity-of-two-numbers.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+a, b = map(int, input().split())
+
+if a % 2 == 0:
+    print('even')
+else:
+    print('odd')
+
+if b % 2 == 0:
+    print('even')
+else:
+    print('odd')
