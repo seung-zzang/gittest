4ce2f78cc4dcdfcad3b027b0a4ec17fb66c6f85e
seung-zzang <164981656+seung-zzang@users.noreply.github.com>
2025-11-27 10:56:51 +0900

    Title: 특정 조건 두 정수 비교, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251127/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+A, B = map(int, input().split())
+
+if A < B:
+    print(1, end = ' ')
+else:
+    print(0, end = ' ')
+
+if A == B:
+    print(1)
+else:
+    print(0)
