ba1ab6faef5f88ad48f935379fca03a4f001e5c8
seung-zzang <164981656+seung-zzang@users.noreply.github.com>
2025-11-27 11:08:15 +0900

    Title: 남녀노소 구분짓기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251127/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+sex = int(input())
+age = int(input())
+
+if sex == 0:
+    if age >= 19:
+        print('MAN')
+    else:
+        print('BOY')
+
+if sex == 1:
+    if age >= 19:
+        print('WOMAN')
+    else:
+        print('GIRL')
